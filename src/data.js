75c8105b6ae8310d1d277fd0261419e37b7f0e33
LujanSolo <lujansolo@proton.me>
2022-12-06 20:50:21 -0800

    updated resume and about. troubleshooting broken images.

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Orbital Odyssey",
+    img: "odyssey.png",
+    desc: "Educational app that provides data on the planets of the Solar System, with secure login and client database for note keeping.",
+    online: "https://stormy-sands-64982.herokuapp.com/",
+    github: "https://github.com/Proper-Stevo/SpaceInvaders",
+  },
+  {
+    id: 2,
+    title: "Impulse!",
+    img: "impulse.png",
+    desc: "A full-stack browser-board game where impulse shopping must be leveraged against money and happiness until the finish line is reached.",
+    online: "https://impulse-the-game.herokuapp.com/",
+    github: "https://github.com/LujanSolo/Impulse",
+  },
+  {
+    id: 3,
+    title: "The Catered Gamer",
+    img: "catered-gamer.png",
+    desc: "Discover and re-discover games from all eras based on user's genre preference and type of gaming platform.",
+    online: "https://camilleyong.github.io/the-catered-gamer/",
+    github: "https://github.com/camilleyong/the-catered-gamer",
+  }
+]
