1ab6dd5a13fef2a7a612eee7cd4b2d52a675967f
LujanSolo <lujansolo@proton.me>
2022-12-06 20:28:48 -0800

    added functionality to portfolioContainer, added pictures files/folder into assets folder updated stylesheet and bootstrap cdn

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="name-container">
+        <h1 className="my-name">Jamie Lujan</h1>
+        <p className="my-title">Full-Stack Web Developer</p>
+        <p className="alma-mater">UCLA Class of '22</p>
+      </div>
+    </header>
+  )
+}
