891e438b2d8f2267c714caa37d3be99d230f3bfa
LujanSolo <lujansolo@proton.me>
2022-12-14 15:11:21 -0800

    added github and linkedin components to homepage for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header';
+import Home from './components/pages/Home';
 import PortfolioContainer from './components/PortfolioContainer';
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <>
     <Header />
+    <Home />
     <PortfolioContainer />
     </>
     
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,19 @@
-import gitIcon from '../../assets/images/github.png';
-import linkedIcon from '../../assets/images/linkedin.png'
-
+export default function Home() {
+  return (
+    <>
+      <div className='grid1'>
+        <img src={require('../../assets/images/github.png')} className='git-icon' />
+        <img src={require('../../assets/images/linkedin.png')} className='git-icon' />
+      </div>
+      <div className='grid2'>
+        
+      </div>
+      <div className='grid3'>
+        
+      </div>
+      <div className='grid4'>
+                
+      </div>
+    </>
+  )
+}
